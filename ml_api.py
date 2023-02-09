05cabd2d27e51d51691370b9d459a356afa5270d
Shu Liu <shul.max9303@gmail.com>
2023-02-09 20:24:28 +0000

    save the model in pickle file and have it running on fastapi

--- ml_api.py
+++ ml_api.py
@@ -1,3 +1,6 @@
+import pickle
+import pandas as pd
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -12,8 +15,14 @@ class HouseData(BaseModel):
     TotRmsAbvGrd: int
     YearBuilt: int
     YearRemodAdd: int
-    Firplaces: int
+    Fireplaces: int
+
+with open('xgbrmodel.pkl', 'rb') as xgbr:
+    model = pickle.load(xgbr)
 
 @app.post('/')
 async def price_endpoint(item:HouseData):
-    return item
+    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
+
+    y_pred = model.predict(df)
+    return {"SalePrice": int(y_pred)}
